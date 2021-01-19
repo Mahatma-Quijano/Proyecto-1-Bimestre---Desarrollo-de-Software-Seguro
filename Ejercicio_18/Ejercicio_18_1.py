3c57c2c2721f9bdf8dd53797b90fdf6cf1e3cf6a
Mahatma <45208476+MahatmaQ@users.noreply.github.com>
2021-01-18 20:28:53 -0500

    Hasta 18.2

--- Ejercicio_18/Ejercicio_18_1.py
+++ Ejercicio_18/Ejercicio_18_1.py
@@ -1,33 +1,40 @@
+# Método que realiza el cálculo de izquierda a derecha
 def calculate(line):
-    ans = 0
-    i = 0
-    op = ''
-    while i < len(line):
+    ans = 0 # Acumulador del resultado
+    i = 0   # Iterador
+    op = '' # Operador
+    while i < len(line): # Mientras la línea tenga más de 1 símbolo
         if line[i] == ')':
-            return ans, i
+            return ans, i # Retorna el valor acumulado y la última posición del iterador
         if line[i] in ['+', '*']:
-            op = line[i]
+            op = line[i] # Almacena el operador
         elif line[i].isnumeric():
-            if ans == 0:
+            if ans == 0: # Si el acumulador aún no ha sido actualizado
                 ans = int(line[i])
             else:
                 b = int(line[i])
-                ans = ans * b if op =='*' else ans + b
-        elif line[i] == '(':
+                ans = ans * b if op == '*' else ans + b # Se actualiza el resultado
+        elif line[i] == '(': # Con cada paréntesis se utiliza recursividad
             b, j = calculate(line[i+1:])
-            ans = ans * b if op =='*' else ans + b
-            i += j + 1
+            ans = ans * b if op == '*' else ans + b
+            i += j + 1 # El iterador se actualiza a la posición al finalizar el paréntesis
         i += 1
-    return ans, i
+    return ans, i # Retorna el valor acumulado y la última posición del iterador
 
 
 total = 0
+# Se obtienen los datos del archivo de entrada
 with open("inputDiana.txt") as file:
     for line in file:
+        # Se añaden espacios a los paréntesis
+        # de manera que cada signo esté separado por espacios
         line = line.rstrip().replace('(', '( ')
         line = line.replace(')', ' )')
+        # Se crea una lista de todos los símbolos
         expression = line.split()
+        # Se obtiene el cálculo
         result, _ = calculate(expression)
+        # Se acumula la suma de cada resultado
         total += result
 
 print("TOTAL:", total)
--- Ejercicio_18/Ejercicio_18_2.py
+++ Ejercicio_18/Ejercicio_18_2.py
@@ -1,94 +1,53 @@
-import re
+import re # Importa la librería de Expresiones Regulares
 
-def calculate2(line):
-    ans, i, op = 0, 0, ''
-    while i < len(line):
-        flag = '+' not in line
-        if line[i] == '+':
-            op = line[i]
-        elif line[i] == '*':
-            if flag:
-                op = line[i]
-            else:
-                ans = 0
-        elif line[i].isnumeric():
-            if ans == 0:
-                ans = int(line[i])
-            elif op != '':
-                a = ans
-                b = int(line[i])
-                if op == '+':
-                    ans += b
-                elif op == '*':
-                    ans *= b
-                line[i] = str(ans)
-                del line[i-2:i]
-                ans = 0
-                i -= 3
-
-        i += 1
-        '''
-        if '+' not in line:
-            i = 0
-        '''
-        if '+' not in line and not flag:
-            i = 0
-
-    return ans, i
-
+# Método que calcula las operaciones de una expresion sin paréntesis
 def calculate(line):
-    ans, i, op = 0, 0, ''
-    while i < len(line):
-        flag = '+' not in line
+    ans, i, op = 0, 0, ''   # Variables iniciales
+    while i < len(line):    # Mientras la línea tenga más de 1 símbolo
+        flag = '+' not in line  # Bandera que indica que se acabaron las sumas
         if line[i] == '+':
             op = line[i]
         elif line[i] == '*':
-            if flag:
+            if flag: # Si ya no hay más sumas, continúa con las multiplicaciones
                 op = line[i]
-            else:
+            else: # No toma en cuenta los números que deban multiplicarse
                 ans = 0
         elif line[i].isnumeric():
-            if ans == 0:
+            if ans == 0:    # Si el acumulador aún no ha sido actualizado
                 ans = int(line[i])
-            elif op != '':
-                a = ans
+            else:
                 b = int(line[i])
+                # Se realiza la operación
                 if op == '+':
                     ans += b
                 elif op == '*':
                     ans *= b
-                line[i] = str(ans)
-                del line[i-2:i]
-                ans = 0
-                i -= 3
+                # ------------------ Se acorta la expresión
+                line[i] = str(ans)  # Se guarda el resultado
+                del line[i-2:i]     # Se eliminan los dos símbolos anteriores
+                ans = 0             # Indica que espera otro número para operar
+                i -= 3              # Retrocede el iterador
         i += 1
+        # La primera vez que se terminen las sumas
         if '+' not in line and not flag:
-            i = 0
-
+            i = 0   # El iterador vuelve al inicio para empezar con las multiplicaciones
     return ans, i
 
-
-
-# TODO que no reciba la lista sino solo el string...
 def operate(exp):
-    string = ''.join(map(str, exp))
-    parenth = re.findall("\([0-9]+[^()]+\)", string)
+    parenth = re.findall("\([0-9]+[^()]+\)", exp)
     while len(parenth) >= 1:
         for p in parenth:
             content = re.findall("([0-9]+|[+*])", p)
             result, i = calculate(content)
-            string = string.replace(p, str(result), 1)
-        exp = list(string)
-        parenth = re.findall("\([0-9]+[^()]+\)", string)
-    content = re.findall("([0-9]+|[+*])", string)
+            exp = exp.replace(p, str(result), 1)
+        parenth = re.findall("\([0-9]+[^()]+\)", exp)
+    content = re.findall("([0-9]+|[+*])", exp)
     return calculate(content)
 
 total = 0
-with open("inputDiana.txt") as file:
+with open("inputMahatma.txt") as file:
     for line in file:
-        line = line.rstrip().replace('(', '( ')
-        line = line.replace(')', ' )')
-        expression = line.split()
+        expression = line.replace(' ', '')
         result, _ = operate(expression)
         total += result
 
